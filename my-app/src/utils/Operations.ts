56eb6c264ad00e0c2594128263942bbe5bc09956
Thomas Caudrelier <thomas.caudrelier@outlook.fr>
2019-03-29 00:28:36 +0100

    Add unit test for ts file

--- my-app/src/utils/Operations.ts
+++ my-app/src/utils/Operations.ts
@@ -2,4 +2,13 @@ function add(x: number, y: number): number {
 	return x + y;
 }
 
-export { add };
+function substract(x: number, y: number): number {
+	return x - y;
+}
+
+function multiply(x: number, y: number): number {
+	// pour la démo sur les tests en continu
+	return 0;
+}
+
+export { add, substract, multiply };
---
+++ my-app/tests/src/utils/Operation.test.ts
@@ -0,0 +1,24 @@
+import { add, substract, multiply } from '../../../src/utils/Operations';
+import { expect } from 'chai';
+import 'mocha';
+
+describe('add function', () => {
+	it('should return 5', () => {
+		const result = add(3,2);
+		expect(result).to.equal(5);
+	});
+});
+
+describe('substract function', () => {
+	it('should return 1', () => {
+		const result = substract(3,2);
+		expect(result).to.equal(1);
+	});
+});
+
+describe('multiply function', () => {
+	it('should return 30', () => {
+		const result = multiply(5,6);
+		expect(result).to.equal(30);
+	});
+});
