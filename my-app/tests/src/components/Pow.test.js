f9b725db76e26fc8dc89a64ea33c6a507f19de92
Thomas Caudrelier <thomas.caudrelier@outlook.fr>
2019-04-01 21:01:15 +0200

    Use jsdom to simulate dom in order to use enzyme's mount function

--- my-app/tests/src/components/Pow.test.js
+++ my-app/tests/src/components/Pow.test.js
@@ -1,20 +1,11 @@
 import React from 'react';
 import { expect } from 'chai';
 import { mount } from 'enzyme';
-
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-configure({ adapter: new Adapter() });
-
-import chai from 'chai'
-import chaiEnzyme from 'chai-enzyme'
-chai.use(chaiEnzyme()) 
-
 import Pow from '../../../src/components/Pow';
 
 describe('<Pow />', () => {
-	it('renders div with class named operation', () => {
+	it('state should be composed of 2 elements', () => {
 		const wrapper = mount(<Pow />);
-		expect(wrapper.contains(<div className="operation"/>)).to.equal(true);
-	});
+    	expect(wrapper.state.length == 2);
+    });
 });
